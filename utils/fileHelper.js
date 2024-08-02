492f009b75749126687f35a5a9acd7d8821a0259
rajastra <rajasaputeraxii.ipa5@gmail.com>
2024-08-02 15:17:35 +0700

    refactor: update public_id prefix in fileHelper.js

--- utils/fileHelper.js
+++ utils/fileHelper.js
@@ -9,7 +9,7 @@ const upload = async (fileBuffer, oldPublicId) => {
 
   const options = {
     resource_type: 'image',
-    public_id: `spbe/images/${Date.now()}`,
+    public_id: `gama/images/${Date.now()}`,
   };
 
   // Delete the old image if a public ID is provided
