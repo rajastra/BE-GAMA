1346e0507c02d1d17c22488d82af7536100fce4a
iqbalalfarizi <iqbal2905h@gmail.com>
2024-12-28 21:09:24 +0700

    add contoller to handle izin status update

--- controllers/izinController.js
+++ controllers/izinController.js
@@ -1,11 +1,45 @@
+/* eslint-disable no-await-in-loop */
 const { Op } = require('sequelize');
+const moment = require('moment');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-
 const Izin = require('../models/izinModel');
 const handlerFactory = require('./handlerFactory');
 const Pegawai = require('../models/pegawaiModel');
+const Presensi = require('../models/presensiModel');
 require('dotenv').config();
+
+const createPresensiWithRangeDate = async (izinData) => {
+  const tglMulai = moment(izinData.tgl_mulai);
+  const tglSelesai = moment(izinData.tgl_selesai);
+  const totalIzin = tglSelesai.diff(tglMulai, 'days') + 1;
+
+  for (let i = 0; i < totalIzin; i) {
+    const currentDate = moment(tglMulai).add(i, 'days');
+
+    if (currentDate.days() !== 0 && currentDate.days() !== 6) {
+      const presensiData = {
+        pegawaiId: izinData.pegawaiId,
+        tgl_absensi: currentDate.format('YYYY-MM-DD'),
+        status: izinData.jenis,
+        lampiran: izinData.lampiran,
+      };
+      console.log(presensiData);
+
+      try {
+        await Presensi.create(presensiData);
+      } catch (error) {
+        console.error(
+          `Failed to create presensi for date ${currentDate.format(
+            'YYYY-MM-DD'
+          )}:`,
+          error
+        );
+        throw new AppError('Failed to create attendance records', 500);
+      }
+    }
+  }
+};
 
 exports.getAllIzin = catchAsync(async (req, res, next) => {
   const { page = 1, limit = 10, keyword = '' } = req.query;
@@ -77,11 +111,36 @@ exports.updateIzin = catchAsync(async (req, res, next) => {
   const izin = await Izin.findByPk(req.params.id);
 
   if (!izin) {
-    return next(new AppError('No document found with that ID', 404));
+    return next(new AppError('No permission record found with that ID', 404));
   }
 
-  // Update the berita record with the new data
   await izin.update(req.body);
+
+  res.status(200).json({
+    status: 'success',
+    data: izin,
+  });
+});
+
+exports.updateStatusIzin = catchAsync(async (req, res, next) => {
+  const { status } = req.body;
+
+  const izin = await Izin.findByPk(req.params.id, {
+    include: [{ model: Pegawai }],
+  });
+  if (!izin) {
+    return next(new AppError('No permission record found with that ID', 404));
+  }
+
+  if (status === 'disetujui') {
+    try {
+      await createPresensiWithRangeDate(izin);
+    } catch (error) {
+      return next(new AppError('Failed to create attendance records', 500));
+    }
+  }
+
+  await izin.update({ status });
 
   res.status(200).json({
     status: 'success',
@@ -99,7 +158,7 @@ exports.getIzin = catchAsync(async (req, res, next) => {
   });
 
   if (!izin) {
-    return next(new AppError('No document found with that ID', 404));
+    return next(new AppError('No permission record found with that ID', 404));
   }
 
   res.status(200).json({
