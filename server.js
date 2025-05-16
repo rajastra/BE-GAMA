db20a739135adbdd44a57b8f18f8279ab8b60d5e
rajastra <rajasaputeraxii.ipa5@gmail.com>
2025-05-16 16:43:45 +0700

    Refactor server.js to comment out unused HTTPS server code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 const dotenv = require('dotenv');
 
 process.on('uncaughtException', (err) => {
@@ -13,22 +13,22 @@ dotenv.config({
 });
 const app = require('./app');
 
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/apps-api-gama.shop/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/apps-api-gama.shop/fullchain.pem')
-};
+// const options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/apps-api-gama.shop/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/apps-api-gama.shop/fullchain.pem')
+// };
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`Server running on https://localhost:${PORT}`);
-});
+// https.createServer(options, app).listen(PORT, () => {
+//   console.log(`Server running on https://localhost:${PORT}`);
+// });
 
 // Alert => use this for env development
-// const port = process.env.PORT || 3000;
-// const server = app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}!`);
-// });
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! Shutting down...');
